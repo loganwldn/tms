030be13e3e7b47502971b9e9cbc1aa43283ff35a
logan.wldn <logan.wldn2022@gmail.com>
2024-09-03 17:43:45 +0100

    New ticket table

--- database/schema.sql
+++ database/schema.sql
@@ -2,5 +2,16 @@ CREATE TABLE IF NOT EXISTS accounts (
     account_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     username varchar(255),
     password varchar(255),
-    account_type varchar(255)
-)
+    is_admin boolean
+);
+
+-- NEW QUERY --
+
+CREATE TABLE IF NOT EXISTS tickets (
+    ticket_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    ticket_owner_id INTEGER,
+    title varchar(255),
+    content varchar(255),
+    post_date timestamp, 
+    FOREIGN KEY(ticket_owner_id) REFERENCES accounts(account_id)
+);
