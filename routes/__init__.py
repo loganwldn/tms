588bf3a56fa6071c4720d1097c863b4257c064ae
logan.wldn <logan.wldn2022@gmail.com>
2024-09-03 17:43:54 +0100

    Ticket routes

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,8 +2,10 @@ from flask import Blueprint
 
 from .home import home_router
 from .auth import auth_router
+from .tickets import tickets_router
 
 ROUTES: list[Blueprint] = [
     home_router,
-    auth_router
+    auth_router,
+    tickets_router
 ]
