c865ae036e74c4d4086ff2b236088f04cc05c90f
logan.wldn <logan.wldn2022@gmail.com>
2024-09-27 10:52:31 +0100

    Default DB rows for first start

---
+++ database/rows.sql
@@ -0,0 +1,37 @@
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (1, 'First ticket!', 'This is a random ticket.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (2, 'Windows 11 Lenovo Laptop broken', 'My Lenovo laptop running Windows 11 will no longer charge.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (1, 'Phone not turning on', 'The company phone isnt turning on.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (2, 'Printer not responding', 'The printer never responds when I send it something to print.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (1, 'Wi-Fi connection issues', 'The Wi-Fi on my laptop keeps cutting out when I move into my meeting room.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (2, 'Need a new version of VSCode on my laptop', 'Hey all, I need a feature in the newest version of VScode, could you update it for me?', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (1, 'Forgotten password', 'My laptop has locked me out due to a forgotten password. Please reset it for me.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (2, 'Email not syncing', 'My emails between my work phone and laptop are not syncing. I dont get anything through on my phone.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (1, 'Monitor display issues', 'Hi, my monitor is flickering and sometimes blank for a few seconds.', ?)
+
+-- NEW ROW --
+
+INSERT INTO tickets(ticket_owner_id, title, content, post_date) VALUES (2, 'Chrome crashing', 'Hello IT, my laptop seems to not like Chrome anymore since it has crashed many times in the last few days - anything I can do?', ?)
